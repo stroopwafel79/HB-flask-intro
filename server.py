7825d9d58020d2677b3b27810ce1236402ced45d
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-03 12:56:08 -0700

    Added diss function and redirect based on response in hello funct.

--- server.py
+++ server.py
@@ -2,23 +2,22 @@
 
 from random import choice
 
-from flask import Flask, request
+from flask import Flask, request, redirect
 
 # "__name__" is a special Python variable for the name of the current module
 # Flask wants to know this to know what any imported things are relative to.
 app = Flask(__name__)
 
-AWESOMENESS = [
-    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
-    'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
-    'wonderful', 'smashing', 'lovely']
-
-
 @app.route("/")
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return """<!doctype html>
+              <html>
+                <body>Hi! This is the home page. 
+                  <a href="http://localhost:5000/hello">Link hello</a>
+                </body>
+              </html>"""
 
 
 @app.route("/hello")
@@ -34,7 +33,15 @@ def say_hello():
       <body>
         <h1>Hi There!</h1>
         <form action="/greet">
-          What's your name? <input type="text" name="person">
+          What's your name? <input type="text" name="person"><br>
+          Choose your compliment 
+            <select name="compliment">
+                <option value="smart">Smart</option>
+                <option value="creative">Creative</option>
+                <option value="bombass at coding">Bombass At Coding</option>
+                <option value="nice">Nice</option>
+            </select><br>
+          Diss me instead <input type="checkbox" name="diss" value="yes"><br>
           <input type="submit" value="Submit">
         </form>
       </body>
@@ -44,28 +51,48 @@ def say_hello():
 
 @app.route("/greet")
 def greet_person():
-    """Get user by name."""
-
+    """Greet user by name."""
+    diss = request.args.get("diss")
     player = request.args.get("person")
+    compliment = request.args.get("compliment")
+ 
+    if diss:
+        return redirect("/diss")
+    else:    
+        return """
+        <!doctype html>
+        <html>
+          <head>
+            <title>A Compliment</title>
+          </head>
+          <body>
+            Hi, {}! I think you're {}!
+          </body>
+        </html>
+        """.format(player, compliment)
 
-    compliment = choice(AWESOMENESS)
+@app.route("/diss")
+def diss_person():
+    """Insult user by name"""
+    
+    diss = choice(["crappy person", "stupid", "smelly", "funny looking"])
 
-    y = x
-
-    return """
+    return"""
     <!doctype html>
     <html>
       <head>
-        <title>A Compliment</title>
+        <title>A Diss</title>
       </head>
       <body>
-        Hi, {}! I think you're {}!
+        Hi, I think you're {}!
       </body>
     </html>
-    """.format(player, compliment)
+    """.format(diss)
+
+
 
 
 if __name__ == "__main__":
     # debug=True gives us error messages in the browser and also "reloads"
     # our web app if we change the code.
-    app.run(debug=False, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
